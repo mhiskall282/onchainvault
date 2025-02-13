57bed620bb6d3bbae297de133a66d59226dc9b57
Maa-ly <lydiaahenkorahgyamfi@gmail.com>
2025-02-13 15:08:05 -0800

    entryOnGoing

--- contract/src/VaultFactory.sol
+++ contract/src/VaultFactory.sol
@@ -3,13 +3,16 @@ pragma solidity 0.8.28;
 import "./vault.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/clones.sol";
+import {IEntry} from "./interfaces/IEntry.sol";
 
 contract VaultFactory is Ownable {
     using Clones for address; 
     Vault private vault;
+    IEntry private entry;
 
-    constructor() Ownable(msg.sender) {
+    constructor(address entryAddress) Ownable(msg.sender) {
         vault = new Vault(msg.sender);
+        entry = IEntry(entryAddress);
     }
 
     struct UserProfile {
@@ -23,5 +26,6 @@ contract VaultFactory is Ownable {
 
     function createVault() public {
         address newVault = address(vault).clone();
+        
     }
 }
--- contract/src/entry.sol
+++ contract/src/entry.sol
@@ -1,7 +1,83 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+import{ CreateGroup, UserProfile} from "../src/dataStructures/DataStructure.sol";
+import VaultFactory
+contract Entry 
+using DataStructure for DataStructure.CreateGroup;
+using DataStructure for DataStructure.UserProfile;
+
+ mapping(address => UserProfile) public users;
+ mapping(address => (address => bool)) private access;d
+ mapping(address => address[]) private requests;
+ mapping(string => bool) private usernames;
+ 
+ modifier isNewUser(address){
+    require(!users[msg.sender], "Entry__Can_Not_Register_Same_account_Twice");
+ }
 
 
-contract Entry{
+constructor(){}
+function connect(string memory _userName, uint256 _tokenId, PRIVACY _privacy) external isNewUser(msg.sender) {
+   require(!usernames[_userName],"Entry__UserName_Taken");
+ 
+    userProfile =  UserProfile({
+        username : _userName,
+        tokenId : _tokenId,
+        owner: msg.sender,
+        vaults:  ,
+        privacy: _privacy
+    });
+    usernames[_username] = true;
+}
+
+function addVault(address _user, address _vaultAddress) internal onlyAuthorized {
+   users[_user].vaults.push(_vaultAddress);
+}
+
+
+function  createGroup(string memory  _name, address[] members,uint256 _tokenId, PRIVACY _privacy) external {
     
+    createGroup = CreateGroup({
+        name: _name,
+        members: _tokenId,
+        tokenId : _tokenId,
+        vaults: , 
+        privacy: _privacy
+    })
+}
+
+function updateUserProfile(string memory  _username, uint256 _tokenId, PRIVACY _privacy) external{
+   user[msg.sender].username = _username;
+   user[msg.sender].tokenId = _tokenId;
+   user[msg.sender].privacy = _privacy;
+}
+
+function  getProfile() external{
+   return users[msg.sender];
+}
+
+
+function requestAccessToProfile(address _user) internal {
+   // access[msg.sender][_user] = true;
+   requests[_user].push(msg.sender);
+
+}
+
+
+ function acceptRequest(address _user) external {
+   address[] _requests = requests[msg.sender];
+   for (uint256 i; i<_requests.length; i++) {
+      if (_requests[i] == _user) {
+         access[msg.sender][_user] = true;
+      }
+   }
+ }
+ 
+
+function removeFromProfileAccess(address _user) external{
+   access[msg.sender][_user] = false;
+}
+
+function updateProfilePicture(uint256 _tokenId) external{
+   user[msg.sender].tokenId = _tokenId;
 }
